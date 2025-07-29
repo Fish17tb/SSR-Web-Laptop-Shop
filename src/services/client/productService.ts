994d1d782384d6567d97a10e67425825db734a1b
Hoàng Nguyên Vũ <fish17tb@gmail.com>
2025-07-29 13:36:11 +0700

    Add-Function-Filter-And-Sorting-Product-By-Condition

--- src/services/client/productService.ts
+++ src/services/client/productService.ts
@@ -21,6 +21,87 @@ const getListProducts = async (page: number) => {
   return products;
 };
 
+const getProductWithFilter = async (
+  page: number,
+  factory: string,
+  target: string,
+  price: string,
+  sort: string
+) => {
+  const pageSize = TOTAL_ITEMS_PER_PAGE_CLIENT_PRODUCT;
+  const skip = (page - 1) * pageSize;
+
+  // build where query
+  let whereClause: any = {};
+
+  if (factory) {
+    const factoryInput = factory.split(",");
+    whereClause.factory = {
+      in: factoryInput,
+    };
+  }
+
+  if (target) {
+    const targetInput = target.split(",");
+    whereClause.target = {
+      in: targetInput,
+    };
+  }
+
+  if (price) {
+    const priceInput = price.split(",");
+    // ["duoi-10-trieu", "10-15-trieu", "15-20-trieu", "tren-20-trieu"]
+
+    const priceCondition = [];
+
+    for (let i = 0; i < priceInput.length; i++) {
+      if (priceInput[i] === "duoi-10-trieu") {
+        priceCondition.push({ price: { lt: 10000000 } });
+      }
+      if (priceInput[i] === "10-15-trieu") {
+        priceCondition.push({ price: { gte: 10000000, lte: 15000000 } });
+      }
+      if (priceInput[i] === "15-20-trieu") {
+        priceCondition.push({ price: { gte: 15000000, lte: 20000000 } });
+      }
+      if (priceInput[i] === "tren-20-trieu") {
+        priceCondition.push({ price: { gt: 20000000 } });
+      }
+    }
+
+    whereClause.OR = priceCondition;
+  }
+
+  // build sort
+  let orderByClause: any = {};
+  if (sort) {
+    if (sort === "gia-tang-dan") {
+      orderByClause = {
+        price: "asc",
+      };
+    }
+    if (sort === "gia-giam-dan") {
+      orderByClause = {
+        price: "desc",
+      };
+    }
+  }
+
+  const [products, count] = await prisma.$transaction([
+    prisma.product.findMany({
+      skip: skip,
+      take: pageSize,
+      where: whereClause,
+      orderBy: orderByClause,
+    }),
+    prisma.product.count({ where: whereClause }),
+  ]);
+
+  const totalPages = Math.ceil(count / pageSize);
+
+  return { products, totalPages };
+};
+
 const getProductById = async (id: number) => {
   const product = await prisma.product.findUnique({
     where: {
@@ -155,6 +236,19 @@ const handleDeleteProductInCart = async (
     });
   }
 };
+
+const filterProduct = async (minPrice: string, maxPrice: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      price: {
+        lte: +maxPrice, // lấy các sản phẩm có price <= 15.000.000
+      },
+    },
+  });
+
+  return products;
+};
+
 export {
   getListProducts,
   getProductById,
@@ -162,4 +256,6 @@ export {
   getProductInCart,
   handleDeleteProductInCart,
   countTotalProductPage,
+  filterProduct,
+  getProductWithFilter,
 };
